1857ae0b5b11d4395ca4a144c0f1970b1e5a9c08
GrandNewStart <hbjw123@naver.com>
2023-07-13 22:26:24 +0900

    .

--- Week 1/백준 문제풀이/문자열/10.다이얼.java
+++ Week 1/백준 문제풀이/문자열/10.다이얼.java
@@ -1,24 +1,47 @@
 // https://www.acmicpc.net/problem/5622
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Vector;
+
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        List<Integer> array = new Vector();
-        for (int i=0; i<N; i++) {
-            int x = scanner.nextInt();
-            array.add(x);
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('A',3);
+        map.put('B',3);
+        map.put('C',3);
+        map.put('D',4);
+        map.put('E',4);
+        map.put('F',4);
+        map.put('G',5);
+        map.put('H',5);
+        map.put('I',5);
+        map.put('J',6);
+        map.put('K',6);
+        map.put('L',6);
+        map.put('M',7);
+        map.put('N',7);
+        map.put('O',7);
+        map.put('P',8);
+        map.put('Q',8);
+        map.put('R',8);
+        map.put('S',8);
+        map.put('T',9);
+        map.put('U',9);
+        map.put('V',9);
+        map.put('W',10);
+        map.put('X',10);
+        map.put('Y',10);
+        map.put('Z',10);
+        String S = scanner.nextLine();
+        char[] chars = S.toCharArray();
+        int sum = 0;
+        for (char c : chars) {
+            sum += map.get(c);
         }
-        int v = scanner.nextInt();
-        int count = 0;
-        for (int item : array) {
-            if (item == v) {
-                count++;
-            }
-        }
-        System.out.println(count);
+        System.out.println(sum);
     }
+
 }
--- Week 1/백준 문제풀이/문자열/11.그대로 출력하기.java
+++ Week 1/백준 문제풀이/문자열/11.그대로 출력하기.java
@@ -1,25 +1,15 @@
-// https://www.acmicpc.net/problem/10807
+// https://www.acmicpc.net/problem/11718
 
-import java.util.List;
 import java.util.Scanner;
-import java.util.Vector;
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        List<Integer> array = new Vector();
-        for (int i=0; i<N; i++) {
-            int x = scanner.nextInt();
-            array.add(x);
+        while (scanner.hasNext()) {
+            String S = scanner.nextLine();
+            System.out.println(S);
         }
-        int v = scanner.nextInt();
-        int count = 0;
-        for (int item : array) {
-            if (item == v) {
-                count++;
-            }
-        }
-        System.out.println(count);
     }
+
 }

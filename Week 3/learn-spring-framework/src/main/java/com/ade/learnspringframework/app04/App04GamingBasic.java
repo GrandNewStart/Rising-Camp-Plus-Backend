09cd0193df20bc30db75227ae1f18e68f217f8be
GrandNewStart <hbjw123@naver.com>
2023-07-23 17:55:41 +0900

    .

---
+++ Week 3/learn-spring-framework/src/main/java/com/ade/learnspringframework/app04/App04GamingBasic.java
@@ -0,0 +1,21 @@
+package com.ade.learnspringframework.app04;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ade.learnspringframework.app04")
+public class App04GamingBasic {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(App04GamingBasic.class)) {
+            GameRunner gameRunner = context.getBean(GameRunner.class);
+            gameRunner.run();
+        } catch(Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+}
---
+++ Week 3/learn-spring-framework/src/main/java/com/ade/learnspringframework/app04/GamingConsole.java
@@ -0,0 +1,8 @@
+package com.ade.learnspringframework.app04;
+
+public interface GamingConsole {
+    void up();
+    void down();
+    void right();
+    void left();
+}

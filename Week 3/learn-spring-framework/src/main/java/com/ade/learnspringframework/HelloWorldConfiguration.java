cfa5178f77cc585389df3cafda762a77eab1d56e
GrandNewStart <hbjw123@naver.com>
2023-07-23 14:36:41 +0900

    .

--- Week 3/learn-spring-framework/src/main/java/com/ade/learnspringframework/HelloWorldConfiguration.java
+++ Week 3/learn-spring-framework/src/main/java/com/ade/learnspringframework/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.ade.learnspringframework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person (String name, int age, Address address) {}
 record Address (String firstLine, String city) {}
@@ -20,17 +22,25 @@ public class HelloWorldConfiguration {
     }
 
     @Bean
+    @Primary
     public Address address() {
         return new Address("Baker Street", "London");
     }
 
     @Bean
+    @Qualifier("personWithQualifier")
+    public Address address2() {
+        return new Address("Songpa Avenue", "Seoul");
+    }
+
+    @Bean
+    @Primary
     public Person person() {
         return new Person(name(), 20, address());
     }
 
     @Bean (name="person2")
-    public Person personWithParams(String name, int age, Address address) {
+    public Person personWithParams(String name, int age, @Qualifier("personWithQualifier") Address address) {
         return new Person(name, age, address);
     }
 

82259b099946718ef3efe77de2012f3406e03ee2
GrandNewStart <hbjw123@naver.com>
2023-07-25 20:20:45 +0900

    .

--- Week 3/learn-spring-boot/src/main/java/com/ade/springboot/learnspringboot/LearnSpringBootApplication.java
+++ Week 3/learn-spring-boot/src/main/java/com/ade/springboot/learnspringboot/LearnSpringBootApplication.java
@@ -2,6 +2,7 @@ package com.ade.springboot.learnspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class LearnSpringBootApplication {

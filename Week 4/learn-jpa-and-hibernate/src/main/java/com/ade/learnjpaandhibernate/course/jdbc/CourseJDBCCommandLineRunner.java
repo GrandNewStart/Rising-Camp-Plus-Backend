022250d3cd887b852c5a1d242f5392f45c93a057
GrandNewStart <hbjw123@naver.com>
2023-07-30 21:43:35 +0900

    .

--- Week 4/learn-jpa-and-hibernate/src/main/java/com/ade/learnjpaandhibernate/course/jdbc/CourseJDBCCommandLineRunner.java
+++ Week 4/learn-jpa-and-hibernate/src/main/java/com/ade/learnjpaandhibernate/course/jdbc/CourseJDBCCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.ade.learnjpaandhibernate.course.jdbc;
 
+import com.ade.learnjpaandhibernate.course.models.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,12 @@ public class CourseJDBCCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, "Learn AWS Now", "Jeff Bezos"));
+        repository.insert(new Course(2, "Learn Azure Now", "Satya Nadela"));
+        repository.insert(new Course(3, "Learn DevOps Now", "Jinwoo"));
+        repository.deleteById(3);
+        System.out.println(repository.findById(1));
+        System.out.println(repository.findById(2));
     }
 
 }
--- Week 4/learn-jpa-and-hibernate/src/main/java/com/ade/learnjpaandhibernate/course/jdbc/CourseJDBCRepository.java
+++ Week 4/learn-jpa-and-hibernate/src/main/java/com/ade/learnjpaandhibernate/course/jdbc/CourseJDBCRepository.java
@@ -1,6 +1,8 @@
 package com.ade.learnjpaandhibernate.course.jdbc;
 
+import com.ade.learnjpaandhibernate.course.models.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,11 +15,35 @@ public class CourseJDBCRepository {
     private static final String INSERT_QUERY =
             """
                 insert into course (id, name, author)
-                values (1, 'Learn AWS', 'Jeff Bezos');
+                values (?, ?, ?);
             """;
 
-    public void insert() {
-        template.update(INSERT_QUERY);
+    private static final String DELETE_QUERY =
+            """
+                delete from course 
+                where id = ?
+             """;
+
+    private static final String SELECT_QUERY =
+            """
+                select * from course
+                where id = ?
+            """;
+
+    public void insert(Course course) {
+        this.template.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+    }
+
+    public void deleteById(long id) {
+        this.template.update(DELETE_QUERY, id);
+    }
+
+    public Course findById(long id) {
+        return this.template.queryForObject(
+                SELECT_QUERY,
+                new BeanPropertyRowMapper<>(Course.class),
+                id
+        );
     }
 
 }
